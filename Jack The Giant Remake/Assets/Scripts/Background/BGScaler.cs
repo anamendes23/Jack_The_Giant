a00dbc36c3268f4f996b73ccf356b93288ceadbe
anamendes23 <anamendes23@gmail.com>
2018-12-16 21:38:42 -0800

    Added background scrip to resize

---
+++ Jack The Giant Remake/Assets/Scripts/Background/BGScaler.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BGScaler : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Scale the background to fit the camera
+        SpriteRenderer sr = GetComponent<SpriteRenderer>();
+        Vector3 tempScale = transform.localScale;
+
+        float width = sr.sprite.bounds.size.x;
+        float worldHeight = Camera.main.orthographicSize * 2f;
+        float worldWidth = worldHeight * Screen.width / Screen.height;
+
+        float scale = worldWidth / width;
+        tempScale *= scale;
+
+        transform.localScale = tempScale;
+    }
+}
