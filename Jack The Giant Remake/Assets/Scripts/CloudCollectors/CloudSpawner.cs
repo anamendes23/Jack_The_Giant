a765ac4c60de669921ba4d02d2462d752d7f4770
anamendes23 <anamendes23@gmail.com>
2018-12-29 16:57:22 -0800

    coded cloud spawner

--- Jack The Giant Remake/Assets/Scripts/CloudCollectors/CloudSpawner.cs
+++ Jack The Giant Remake/Assets/Scripts/CloudCollectors/CloudSpawner.cs
@@ -23,14 +23,74 @@ public class CloudSpawner : MonoBehaviour
     private GameObject player;
 
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        
+        controlX = 0;
+        SetMinAndMaxX();
+        CreateClouds();
     }
 
-    // Update is called once per frame
-    void Update()
+    void SetMinAndMaxX()
     {
-        
+        Vector3 bounds = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, 0));
+
+        maxX = bounds.x - 0.5f;
+        minX = -bounds.x + 0.5f;
+    }
+
+    void Shuffle(GameObject[] arrayToShuffle)
+    {
+        GameObject temp;
+        int random;
+
+        for (int i = 0; i < arrayToShuffle.Length; i++)
+        {
+            temp = arrayToShuffle[i];
+            random = Random.Range(i, arrayToShuffle.Length -1);
+            arrayToShuffle[i] = arrayToShuffle[random];
+            arrayToShuffle[random] = temp;
+        }
+    }
+
+    void CreateClouds()
+    {
+        Shuffle(clouds);
+        //for first cloud
+        float positionY = 0f;
+        Vector3 temp;
+
+        for (int i = 0; i < clouds.Length; i++)
+        {
+            temp = clouds[i].transform.position;
+
+            temp.y = positionY;
+            
+            //creates a zig zag of clouds
+            if(controlX == 0)
+            {
+                temp.x = Random.Range(0.0f, maxX);
+                controlX = 1;
+            }
+            else if(controlX == 1)
+            {
+                temp.x = Random.Range(0.0f, minX);
+                controlX = 2;
+            }
+            else if (controlX == 2)
+            {
+                temp.x = Random.Range(1.0f, maxX);
+                controlX = 3;
+            }
+            else if (controlX == 3)
+            {
+                temp.x = Random.Range(-1.0f, minX);
+                controlX = 0;
+            }
+
+            lastCloudPositionY = positionY;
+            clouds[i].transform.position = temp;
+            //update positionY
+            positionY -= distanceBetweenClouds;
+        }
     }
 } //Cloud spawner
