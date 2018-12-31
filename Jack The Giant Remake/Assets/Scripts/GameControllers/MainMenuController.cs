30851a9dbc290c72918536939b95bb710dd1955f
anamendes23 <anamendes23@gmail.com>
2018-12-30 16:26:17 -0800

    added score and pause panel

---
+++ Jack The Giant Remake/Assets/Scripts/GameControllers/MainMenuController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene("GamePlay");
+    }
+
+    public void GoToHighScoreMenu()
+    {
+        SceneManager.LoadScene("HighScore");
+    }
+
+    public void GoToOptionsMenu()
+    {
+        SceneManager.LoadScene("OptionsMenu");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void MusicButton()
+    {
+
+    }
+}
