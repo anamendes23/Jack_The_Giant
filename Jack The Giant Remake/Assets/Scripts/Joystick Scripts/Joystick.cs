b2578d41b2b64b42e626dd16a92d0f245570efe3
anamendes23 <anamendes23@gmail.com>
2019-01-01 22:18:58 -0800

    added script for mobile play

---
+++ Jack The Giant Remake/Assets/Scripts/Joystick Scripts/Joystick.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Joystick : MonoBehaviour, IPointerUpHandler, IPointerDownHandler
+{
+    private PlayerMoveJoystick playerMove;
+
+    void Start()
+    {
+        playerMove = GameObject.Find("Player").GetComponent<PlayerMoveJoystick>();
+    }
+
+    //pressing button
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        if(gameObject.name == "Left")
+        {
+            playerMove.SetMoveLeft(true);
+        }
+        else
+        {
+            //right button down
+            playerMove.SetMoveLeft(false);
+        }
+    }
+
+    //releasing button
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        playerMove.StopMoving();
+    }
+}
---
+++ Jack The Giant Remake/Assets/Scripts/Player/PlayerMoveJoystick.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMoveJoystick : MonoBehaviour
+{
+    public float speed = 8f, maxVelocity = 4f;
+
+    //[SerializeField]
+    private Rigidbody2D rb;
+    private Animator anim;
+
+    private bool moveLeft, moveRight;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+    }
+
+    void FixedUpdate()
+    {
+        if (moveLeft)
+            MoveLeft();
+
+        if(moveRight)
+            MoveRight();
+    }
+
+    public void SetMoveLeft(bool moveLeft)
+    {
+        this.moveLeft = moveLeft;
+        this.moveRight = !moveLeft;
+    }
+
+    public void StopMoving()
+    {
+        moveLeft = moveRight = false;
+        anim.SetBool("Walk", false);
+    }
+
+    void MoveLeft()
+    {
+        float forceX = 0f;
+        float vel = Mathf.Abs(rb.velocity.x);
+
+        Vector3 temp = transform.localScale;
+
+        if (vel < maxVelocity)
+        {
+            forceX = -speed;
+        }
+        anim.SetBool("Walk", true);
+        temp.x = -1.3f;
+        transform.localScale = temp;
+
+        rb.AddForce(new Vector2(forceX, 0));
+    }
+
+    void MoveRight()
+    {
+        float forceX = 0f;
+        float vel = Mathf.Abs(rb.velocity.x);
+
+        Vector3 temp = transform.localScale;
+
+        if (vel < maxVelocity)
+        {
+            forceX = speed;
+        }
+        anim.SetBool("Walk", true);
+        temp.x = 1.3f;
+        transform.localScale = temp;
+
+        rb.AddForce(new Vector2(forceX, 0));
+    }
+}//PlayerMoveJoystick
