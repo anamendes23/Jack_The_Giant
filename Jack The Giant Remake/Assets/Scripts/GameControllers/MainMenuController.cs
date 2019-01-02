88f2db806e5a239636f04745e82582fa383c1579
anamendes23 <anamendes23@gmail.com>
2019-01-01 19:05:27 -0800

    added music controller

--- Jack The Giant Remake/Assets/Scripts/GameControllers/MainMenuController.cs
+++ Jack The Giant Remake/Assets/Scripts/GameControllers/MainMenuController.cs
@@ -2,13 +2,33 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenuController : MonoBehaviour
 {
+    [SerializeField]
+    private Button musicButton;
+    [SerializeField]
+    private Sprite[] musicIcons;
+
     // Start is called before the first frame update
     void Start()
     {
         
+    }
+
+    void CheckToPlayMusic()
+    {
+        if(GamePreferences.GetMusicState() == 1)
+        {
+            MusicController.instance.PlayMusic(true);
+            musicButton.image.sprite = musicIcons[1];
+        }
+        else
+        {
+            MusicController.instance.PlayMusic(false);
+            musicButton.image.sprite = musicIcons[0];
+        }
     }
 
     public void StartGame()
@@ -35,6 +55,15 @@ public class MainMenuController : MonoBehaviour
 
     public void MusicButton()
     {
-
+        if(GamePreferences.GetMusicState()==0)
+        {
+            GamePreferences.SetMusicState(1);
+            CheckToPlayMusic();
+        }
+        else if(GamePreferences.GetMusicState() == 1)
+        {
+            GamePreferences.SetMusicState(0);
+            CheckToPlayMusic();
+        }
     }
 }
